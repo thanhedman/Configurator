b6a41497bc48c8d075d5697ad57078a47093f67d
thanhedman <than.hedman@gmail.com>
2014-07-21 19:25:20 -0400

    Added session starts and submit.php

--- processUpload.php
+++ processUpload.php
@@ -1,15 +1,13 @@
 <?php
-include('connection.php');
 include('SessionHelper.php');
-
-CLIENT_ID = '';
+SessionHelper::secSessionStart();
 
 ERROR = 'There was an error with your session, please reload the app and try again.';
 
 if ( $_SESSION['storeHash'] && $_SESSION['accessToken'] ) {
-	$connection = new Connection( CLIENT_ID, $_SESSION['storeHash'], $_SESSION['accessToken'] );
 	//csv processing logic goes here
-	//bigcommerce calls go here
+	//assign views form goes here
 } else {
 	echo ERROR;
 }
+
--- submit.php
+++ submit.php
@@ -3,10 +3,13 @@ include('connection.php');
 include('SessionHelper.php');
 SessionHelper::secSessionStart();
 
+CLIENT_ID = '';
+
 ERROR = 'There was an error with your session, please reload the app and try again.';
 
 if ( $_SESSION['storeHash'] && $_SESSION['accessToken'] ) {
-	//upload form goes here
+	$connection = new Connection( CLIENT_ID, $_SESSION['storeHash'], $_SESSION['accessToken'] );
+	//Bigcommerce calls go here
 } else {
-	header( 'Location: https://applications.intuitsolutions.net/configurator/load.php' );
+	echo ERROR;
 }
